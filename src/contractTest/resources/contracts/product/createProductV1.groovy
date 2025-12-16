d30d12383bc7059a8f10727b74c101787eaebe3e
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2025-12-15 21:17:53 -0300

    Nivel 3 - 4.7. Evoluindo contratos e recursos

--- src/contractTest/resources/contracts/product/createProductV1.groovy
+++ src/contractTest/resources/contracts/product/createProductV1.groovy
@@ -4,19 +4,30 @@ import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
     request {
-        method GET()
+        method POST()
         headers {
             accept 'application/json'
+            contentType 'application/json'
         }
-        url("/api/v1/products/fffe6ec2-7103-48b3-8e4f-3b58e43fb75a")
+        urlPath("/api/v1/products") {
+            body([
+                    name: "Notebook X11",
+                    brand: "Deep Diver",
+                    regularPrice: 1500.00,
+                    salePrice: 1000.00,
+                    enabled: true,
+                    categoryId: "f5ab7a1e-37da-41e1-892b-a1d38275c2f2",
+                    description: "A Gamer Notebook!"
+            ])
+        }
     }
     response {
-        status 200
+        status 201
         headers {
             contentType 'application/json'
         }
         body([
-                id: fromRequest().path(3),
+                id: anyUuid(),
                 addedAt: anyIso8601WithOffset(),
                 name: "Notebook X11",
                 brand: "Deep Diver",
@@ -25,10 +36,10 @@ Contract.make {
                 inStock: false,
                 enabled: true,
                 category: [
-                        id: anyUuid(),
+                        id: "f5ab7a1e-37da-41e1-892b-a1d38275c2f2",
                         name: "Notebook"
                 ],
-                description: "A Gamer Notebook"
+                description: "A Gamer Notebook!"
         ])
     }
 }
